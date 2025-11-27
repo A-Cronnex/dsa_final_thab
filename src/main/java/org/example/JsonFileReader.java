5eb15406015443fa5e93da1c7c0fc72e36ffc0ad
A-Cronnex <aaron18c1@gmail.com>
2025-11-27 15:29:16 +0100

    ty love you all

---
+++ src/main/java/org/example/JsonFileReader.java
@@ -0,0 +1,26 @@
+package org.example;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class JsonFileReader {
+    private List<String> NodeNames;
+    private List<String> Connections;
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(new File("src/sample.json"));
+        JsonNode node= jsonNode.get("nodes");
+        TypeReference<List<Node>> typeRef = new TypeReference<List<Node>>() {};
+
+        List<Node> nodes= objectMapper.readValue(node.traverse(), typeRef);
+
+        System.out.println(nodes.get(0).getId());
+        System.out.println(nodes);
+
+    }
+}
---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Node {
+    private String id;
+    private String type;
+
+    public Node(){}
+
+    public void setId(String id) { this.id = id;}
+    public void setType(String type) {this.type = type;}
+    public String getId() {return this.id;}
+    public String getType() {return this.type;}
+}
