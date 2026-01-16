617fa7bc3bd1780a27b97cbe1b2075aa185108e1
Ivan-Pim <snaivini@yandex.ru>
2026-01-16 14:31:48 +0100

    JSON shenanigans

--- src/main/java/org/example/JSON/JSONEdge.java
+++ src/main/java/org/example/JSON/JSONEdge.java
@@ -3,7 +3,7 @@ package org.example.JSON;
 public class JSONEdge {
     private String from;
     private String to;
-    private int energy;
+    private int energy_cost;
     private int capacity;
     private int distance;
     private boolean bidirectional;
@@ -11,24 +11,24 @@ public class JSONEdge {
 
     public JSONEdge(){}
 
-    public JSONEdge(String from, String to, int capacity, int energy){
+    public JSONEdge(String from, String to, int capacity, int energy_cost){
         this.from = from;
         this.to = to;
         this.capacity = capacity;
-        this.energy = energy;
+        this.energy_cost = energy_cost;
     }
 
-    public JSONEdge(String from, String to, int distance, int capacity, int energy, boolean restricted, boolean bidirectional) {
+    public JSONEdge(String from, String to, int distance, int capacity, int energy_cost, boolean restricted, boolean bidirectional) {
         this.from = from;
         this.to = to;
         this.distance = distance;
         this.capacity = capacity;
-        this.energy = energy;
+        this.energy_cost = energy_cost;
         this.restricted = restricted;
         this.bidirectional = bidirectional;
     }
 
-    public void setEnergy(int energy) {this.energy = energy;}
+    public void setenergy_cost(int energy_cost) {this.energy_cost = energy_cost;}
     public void setCapacity(int capacity) {this.capacity = capacity;}
     public void setDistance(int distance) {this.distance = distance;}
     public void setBidirectional(boolean bidirectional) {this.bidirectional = bidirectional;}
@@ -41,7 +41,7 @@ public class JSONEdge {
     public boolean isBidirectional() {return bidirectional;}
     public int getDistance() {return distance;}
     public int getCapacity() {return capacity;}
-    public int getEnergy() {return energy;}
+    public int getEnergy() {return energy_cost;}
     public String getTo() {return to;}
 
     @Override
@@ -49,7 +49,7 @@ public class JSONEdge {
         return "JSONEdge{" +
                 "from='" + from + '\'' +
                 ", to='" + to + '\'' +
-                ", energy=" + energy +
+                ", energy_cost=" + energy_cost +
                 ", capacity=" + capacity +
                 ", distance=" + distance +
                 ", bidirectional=" + bidirectional +
